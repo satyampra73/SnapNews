4cf04e6660dcd3388272722d2b1f33e8704d389f
Satyam Prajapati <satyam7355@gmail.com>
2025-01-23 23:16:15 +0530

    initial commit

---
+++ app/src/main/java/com/satyam/snapnews/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.satyam.snapnews
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.setPadding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
